60a8019d0819968a8984e6e2e421b2644163177f
Russell Smith <russellsmith@MacBook-Pro.local>
2018-02-22 08:39:47 -0500

    create delete route

--- api/bar/bar.route.js
+++ api/bar/bar.route.js
@@ -6,6 +6,7 @@ const barRouter = express.Router();
 barRouter.get('/', barController.getAllBars);
 barRouter.post('/', barController.createBar);
 barRouter.post('/:barid', barController.addBarAttendee);
+barRouter.delete('/:barid', barController.deleteBar);
 barRouter.delete('/:barid/attendee/:attendeeid', barController.deleteBarAttendee);
 
 module.exports = barRouter;
