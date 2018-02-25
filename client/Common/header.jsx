0f8508fadca1b622080a57f5cc43f7ff57e23ae8
Russell Smith <russellsmith@MacBook-Pro.local>
2018-02-25 15:51:43 -0500

    fix header

--- client/Common/header.jsx
+++ client/Common/header.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../service';
 
-const Header = () => {
+const Header = (props) => {
+  function signOut(){
+    auth.signOut()
+  }
+
+  function renderContent(user){
+    let Content = null;
+    if(user){
+      return Content = (
+        <li><a onClick={signOut} href="#">Sign Out</a></li>
+      )
+    }else {
+      return Content = (
+       <div>
+         <li><Link to="/login">Sign In</Link></li>
+         <li><Link to="/register">Sign Up</Link></li>
+       </div>
+     )
+    }
+  }
+
   return (
 	 <header>
 		<nav className="container">
@@ -9,8 +30,7 @@ const Header = () => {
 		    <Link className="brand" to="/">Night Out <i className="fa fa-glass" aria-hidden="true"></i></Link>
 		   </div>
   			<ul className="navbar-right">
-  				<li><a href="">Sign In</a></li>
-  				<li><a href="">Sign Up</a></li>
+          {renderContent(props.user)}
   			</ul>
 		</nav>
 	</header>
